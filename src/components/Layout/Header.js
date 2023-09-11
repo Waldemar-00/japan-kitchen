b0ea16469464e38a3c07b792299f8bdeafc11d22
uladzimir <01myit@gmail.com>
2023-09-11 18:02:20 +0300

    created Button component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,17 @@
 import styles from './Header.module.css'
 import sushiImg from "../../assets/img/sushi.jpg"
+import Button from '../UI/Button'
+import CartIcon from '../Cart/CartIcon'
 function Header() {
   return (
     <>
       <header className={styles.header}>
         <h1>Japan Kitchen</h1>
-        <button type='button'>Cart</button>
+        <Button type='button'>
+          <div className={styles.div}><CartIcon/></div>
+          <div className={styles.div}>Cart</div>
+          <div className={styles.lastInBtn}>12</div>
+        </Button>
       </header>
       <div className={styles.img}>
         <img src={sushiImg} alt="sushi" />
